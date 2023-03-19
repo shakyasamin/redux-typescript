1d7a4fbaf9aa44690d35e5709f8b8b115765b195
shakyasamin <saminshakya27@gmail.com>
2023-03-19 11:51:54 +0545

    creating type selector calling action creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useTypedSelector } from "../hooks/UseTypeSelector";
+
+
+import { useActions } from "../hooks/useAction";
 
 const RepositoriesList: React.FC = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.repositories);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  };
 
+    searchRepositories(term);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
